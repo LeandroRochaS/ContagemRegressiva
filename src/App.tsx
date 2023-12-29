586354553ac6a3456d7f06b661c3f8b036d24afa
Leandro Rocha Santos <lerocha644@gmail.com>
2023-12-29 00:27:19 -0300

    update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+//import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
 import "./App.css";
 import Count from "./components/Count";
 import countDown from "./scripts/countDown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
+
 import { useState } from "react";
 
 function App() {
@@ -20,23 +23,32 @@ function App() {
     setSecond(textSecond);
   }, 1000);
 
+  // const targetDate = new Date("2023-12-31T24:00:00").getTime();
+
   return (
     <>
-      <div>
-        <div className="container">
-          <div className="contagem-regressiva">
-            <Count title="Dias" numero={`${day}`} />
-            <Count title="Horas" numero={hour < 10 ? `0${day}` : `${day}`} />
-            <Count
-              title="Minutos"
-              numero={minute < 10 ? `0${minute}` : `${minute}`}
-            />
-            <Count
-              title="Segundos"
-              numero={second < 10 ? `0${second}` : `${second}`}
-            />
-          </div>
+      <div className="body">
+        {/* <div className="container">
+          <FlipClockCountdown
+            to={targetDate}
+            style={{ fontSize: "2rem" }}
+            className="flip-clock"
+          /> */}
+        <h1>2024</h1>
+        <h1 className="reverse">2024</h1>
+        <div className="contagem-regressiva">
+          <Count title="Dias" numero={`${day}`} />
+          <Count title="Horas" numero={hour < 10 ? `0${day}` : `${day}`} />
+          <Count
+            title="Minutos"
+            numero={minute < 10 ? `0${minute}` : `${minute}`}
+          />
+          <Count
+            title="Segundos"
+            numero={second < 10 ? `0${second}` : `${second}`}
+          />
         </div>
+        {/* </div> */}
       </div>
     </>
   );
